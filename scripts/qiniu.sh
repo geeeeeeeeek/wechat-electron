e05d07c580a61ebcf7809ce15ed22bf68a6460d0
ray26 <romerxzh@gmail.com>
2016-05-27 22:58:51 +0800

    使用绝对路径

--- scripts/qiniu.sh
+++ scripts/qiniu.sh
@@ -1,7 +1,9 @@
-- wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz
-- tar -xvf qiniu-devtools-linux_amd64-current.tar.gz
-- mkdir output
-- mv dist/mac-osx.tar.gz output
-- mv dist/linux-x64.tar.gz output
-- mv dist/linux-ia32.tar.gz output
-- ./qrsync conf.json
+#!/bin/bash
+
+wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat
+tar -xvf /home/travis/build/geeeeeeeeek/electronic-wechat/qiniu-devtools-linux_amd64-current.tar.gz
+mkdir /home/travis/build/geeeeeeeeek/electronic-wechat/output
+mv /home/travis/build/geeeeeeeeek/electronic-wechat/dist/mac-osx.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
+mv /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-x64.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
+mv /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-ia32.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
+./home/travis/build/geeeeeeeeek/electronic-wechat/qrsync /home/travis/build/geeeeeeeeek/electronic-wechat/conf.json
