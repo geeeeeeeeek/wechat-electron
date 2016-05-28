709e56b1f5301b03f03fa21e57db821742ae8755
ray26 <romerxzh@gmail.com>
2016-05-28 22:07:59 +0800

    use sudo to fix permission

--- scripts/qiniu.sh
+++ scripts/qiniu.sh
@@ -3,7 +3,7 @@
 wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat
 tar -xvf /home/travis/build/geeeeeeeeek/electronic-wechat/qiniu-devtools-linux_amd64-current.tar.gz
 mkdir /home/travis/build/geeeeeeeeek/electronic-wechat/output
-mv /home/travis/build/geeeeeeeeek/electronic-wechat/dist/mac-osx.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
-mv /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-x64.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
-mv /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-ia32.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
+cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/mac-osx.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
+cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-x64.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
+cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-ia32.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
 ./home/travis/build/geeeeeeeeek/electronic-wechat/qrsync /home/travis/build/geeeeeeeeek/electronic-wechat/conf.json
