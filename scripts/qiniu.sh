5d744f10fabd333235f0b910a19fb6fb2858173d
ray26 <romerxzh@gmail.com>
2016-05-28 23:18:56 +0800

    fix wget 路径

--- scripts/qiniu.sh
+++ scripts/qiniu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat
+wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz -P /home/travis/build/geeeeeeeeek/electronic-wechat
 tar -xvf /home/travis/build/geeeeeeeeek/electronic-wechat/qiniu-devtools-linux_amd64-current.tar.gz -C /home/travis/build/geeeeeeeeek/electronic-wechat
 mkdir /home/travis/build/geeeeeeeeek/electronic-wechat/output
 cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/mac-osx.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
